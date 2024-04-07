dd4acda17523e3e8af375570b7ade8236d7e81bf
Marco Antonio <antonperez90@comunidad.unam.mx>
2024-04-06 20:39:26 -0600

    Ṕroject start

---
+++ src/main/java/anton/api/graphql_test_it/GraphqlTestItApplication.java
@@ -0,0 +1,13 @@
+package anton.api.graphql_test_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlTestItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlTestItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anton/api/graphql_test_it/GraphqlTestItApplicationTests.java
@@ -0,0 +1,13 @@
+package anton.api.graphql_test_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlTestItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
